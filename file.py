726b8b49ff5629c9698db2e6d5b5452e8d777468
Fredy Jaimes <fred20ngv@gmail.com>
2026-04-01 17:30:08 -0500

    general changes

---
+++ file.py
@@ -0,0 +1,16 @@
+secret_number = 15
+limit = 4
+chances = 0
+while chances < limit:
+    user_guess = int(input("please input your number: "))
+    chances += 1
+    if user_guess == secret_number:
+        print("You win")
+        break
+    elif user_guess != secret_number:
+        if user_guess % 2 == 0:
+            print("wrong, but the number is even")
+        else:
+            print("wrong, and the number is odd")
+else:
+    print("Game over, the number was 15")
